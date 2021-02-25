a45b7b640b40dd25f28b14d0d2179d9514ebb0f9
Drew Wiens <adwiens@users.noreply.github.com>
2021-02-25 14:07:23 -0600

    Throw errors instead of returning undefined

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ class PostgraphileClient extends GraphQLClient {
       req._setMethod('POST');
       req._setURL('/graphql');
       res.end = ((result) => resolve(JSON.parse(result).data)); // TODO: error handling
+      res.end = (result) => {
+        const json = JSON.parse(result);
+        if (json.errors && json.errors.length > 0) {
+          // "errors" seems to be a GraphQLFormattedErrorExtended[]
+          // (from postgraphile) but with an extra "data" prop
+          reject(json.errors);
+        } else {
+          resolve(json.data);
+        }
+      };
       req.isInternalQuery = true;
       this.postgraphile(req, res);
     });
